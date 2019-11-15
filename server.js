6250ca25420b4df20024e4b16dc5a7b36b805bac
shraddha <moreshraddhasudhakar@gmail.com>
2019-11-15 17:00:48 +0530

    serverjs file load

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/', (request, response) => {
+    response.send('welcome to my application')
+})
